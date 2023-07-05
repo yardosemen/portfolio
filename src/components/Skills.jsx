8e47e4da99dd15022e011b25bbfcb926d7732104
Yaroslav Semianchuk <yardosemen@gmail.com>
2023-07-05 17:00:21 +0300

    add_Work

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,7 @@ import Redux from "../assets/redux.png";
 
 const Skills = () => {
 	return (
-		<div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+		<div name="skills" className="w-full h-full bg-[#0a192f] text-gray-300">
 			{/* Container */}
 			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
 				<div>
---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+import LandIng from "../assets/projects/landing.png";
+import Fitness from "../assets/projects/workinmg.jpeg";
+import Shop from "../assets/projects/shop.png";
+import pixiJs from "../assets/projects/pixiJs.png";
+
+export const data = [
+	{
+		id: 1,
+		name: "React JS Application",
+		image: LandIng,
+		github: "https://github.com/yardosemen/Landing_Tour",
+	},
+	{
+		id: 2,
+		name: "React JS Application",
+		image: Fitness,
+		github: "https://github.com/yardosemen/fitnes",
+	},
+	{
+		id: 3,
+		name: "React JS Redux Application",
+		image: Shop,
+		github: "https://github.com/yardosemen/Shop/tree/master/src",
+	},
+	{
+		id: 4,
+		name: "Java Script and Pixi Js Application",
+		image: pixiJs,
+		github: "https://github.com/yardosemen/pixi",
+	},
+];
