a8bf0bbe879c6269603a67b20b25670b18b918f0
Yaroslav Semianchuk <yardosemen@gmail.com>
2023-07-05 17:28:36 +0300

    add_navedit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,11 +58,31 @@ const Navbar = () => {
 						: "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
 				}
 			>
-				<li className="py-6 text-4xl">Home</li>
-				<li className="py-6 text-4xl">About</li>
-				<li className="py-6 text-4xl">Skills</li>
-				<li className="py-6 text-4xl">Work</li>
-				<li className="py-6 text-4xl">Contact</li>
+				<li className="py-6 text-4xl">
+					<Link onClick={handleClick} to="home" smooth={true} duration={500}>
+						Home
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					<Link onClick={handleClick} to="about" smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					<Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+						Skills
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					<Link onClick={handleClick} to="work" smooth={true} duration={500}>
+						Work
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					<Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+						Contact
+					</Link>
+				</li>
 			</ul>
 
 			{/*social menu*/}
